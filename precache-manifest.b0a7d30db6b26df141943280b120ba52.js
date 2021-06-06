b5123dfab4f005607b4ff2b665688c22ee97b577
WarisAli-source <warissid41@gmail.com>
2021-06-06 16:32:11 +0530

    Updates

---
+++ precache-manifest.b0a7d30db6b26df141943280b120ba52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3004c82de1cffa56285b",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/css/main.1727f1bd.chunk.css"
+  },
+  {
+    "revision": "3004c82de1cffa56285b",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/js/main.0ff11503.chunk.js"
+  },
+  {
+    "revision": "59b368382333e61858d3",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/js/runtime~main.21ed214b.js"
+  },
+  {
+    "revision": "07bf9ab0012b8ebd5b11",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/css/2.879f8213.chunk.css"
+  },
+  {
+    "revision": "07bf9ab0012b8ebd5b11",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/js/2.72c56ea6.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b26b5a32a33c21da6a2b5f9ec8bc4f17",
+    "url": "/WarisAli-source/Final-Project-React-Coursera/index.html"
+  }
+];
