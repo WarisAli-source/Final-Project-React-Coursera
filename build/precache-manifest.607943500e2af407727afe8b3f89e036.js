cba33a912c9e2fdeff2fb4949ad8d70e064423e2
WarisAli-source <warissid41@gmail.com>
2021-06-06 16:24:16 +0530

    Add files via upload

---
+++ build/precache-manifest.607943500e2af407727afe8b3f89e036.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c29c51142283797fb34c",
+    "url": "/static/css/main.1727f1bd.chunk.css"
+  },
+  {
+    "revision": "c29c51142283797fb34c",
+    "url": "/static/js/main.b0bc23c1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3eb3dee20efad9cc582e",
+    "url": "/static/css/2.11da8771.chunk.css"
+  },
+  {
+    "revision": "3eb3dee20efad9cc582e",
+    "url": "/static/js/2.72c56ea6.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "efb85ae8404381b3232e993da041c4a8",
+    "url": "/index.html"
+  }
+];
